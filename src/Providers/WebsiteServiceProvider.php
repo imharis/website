148e4347fdedadfbb10cd99582af246247209dcf
imharis <haris.codingpixcel@gmail.com>
2019-05-06 15:53:13 +0500

    First Commit

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Waygou\Website\Providers;
+
+use illuminate\Support\ServiceProvider;
+
+class WebsiteServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        dd("here we go");
+    }
+    
+    public function register()
+    {
+        
+    }
+}
